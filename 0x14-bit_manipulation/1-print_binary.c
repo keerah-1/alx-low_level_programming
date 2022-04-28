bcdaa62e6c4190f52c310e9ad4d9e71cacf43ec7
Keerah-1 <danaezeko@gmail.com>
2022-04-28 14:58:46 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,17 +1,48 @@
 #include "main.h"
 
 /**
- * get_bit - value of a bit at a given index
- * @n: decimal parameter
- * @index: index
- * Return: val
+ * _pow - calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
+ *
+ * Return: value of (base ^ power)
  */
-int get_bit(unsigned long int n, unsigned int index)
+unsigned long int _pow(unsigned int base, unsigned int power)
 {
-	int bit;
+	unsigned long int num;
+	unsigned int i;
 
-	bit = (n >> index);
-	if (index > 32)
-	return (-1);
-	return (bit & 1);
+	num = 1;
+	for (i = 1; i <= power; i++)
+		num *= base;
+	return (num);
+}
+
+/**
+ * print_binary - prints a number in binary notation
+ * @n: number to print
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int divisor, check;
+	char flag;
+
+	flag = 0;
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
+	{
+		check = n & divisor;
+		if (check == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
+		{
+			_putchar('0');
+		}
+		divisor >>= 1;
+	}
 }
