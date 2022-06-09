40f5f0ccc67b5164e19298d2b62d3e86c5bf872c
Keerah-1 <danaezeuko@gmail.com>
2022-06-09 10:28:55 -0700

    list.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,19 +1,33 @@
-#include "lists.h"
-/**
- * print_dlistint - print a doubly linked list
- * @h: the doubly linked list
- * Return: the length of the double linked list
- */
-size_t print_dlistint(const dlistint_t *h)
-{
-	const dlistint_t *actual = h;
-	size_t len = 0;
+#ifndef LISTS_H
+#define LISTS_H
 
-	while (actual)
-	{
-		printf("%i\n", actual->n);
-		actual = actual->next;
-		len++;
-	}
-	return (len);
-}
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
+#endif /* LISTS_H */
