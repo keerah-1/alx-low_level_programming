417c59863aaefa29c46161b2a7ff7d6a7770f490
Keerah-1 <danaezeuko@gmail.com>
2022-06-13 10:18:37 -0700

    done

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Karenahv/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/injectcode.so
-export LD_PRELOAD=/tmp/injectcode.so
+gcc *.c -c -fPIC
+gcc *.o -shared -o libmain.so
